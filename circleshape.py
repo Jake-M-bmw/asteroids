7e922545f7066500439b1c838673c30061c6ef27
Jake-M-bmw <47jdmiller@gmail.com>
2025-02-23 19:43:16 -0500

    addes asteroids and movement

--- circleshape.py
+++ circleshape.py
@@ -3,13 +3,24 @@ import pygame
 class CircleShape(pygame.sprite.Sprite):
 	def __init__(self, x, y, radius):
 		if hasattr(self, "containers"):
-			super().__init__(self.containers)
+			super().__init__(*self.containers)
 		else:
 			super().__init__()
 
 		self.position = pygame.Vector2(x, y)
 		self.velocity = pygame.Vector2(0, 0)
 		self.radius = radius
+
+	def collision(self, other):
+		distance = self.position.distance_to(other.position)
+		return distance <= self.radius + other.radius
+
+
+
+
+
+
+
 
 	def draw(self, screen):
 		pass
--- main.py
+++ main.py
@@ -2,21 +2,38 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 def main():
 	pygame.init()
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroid = pygame.sprite.Group()
+
+	Player.containers = (updatable, drawable)
+	AsteroidField.containers = (updatable,)
+	Asteroid.containers = (asteroid, updatable, drawable)
+
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	asteroid_field = AsteroidField()
 
 	while True:
 		screen.fill("black")
-		player.update(dt)
-		player.draw(screen)
+		updatable.update(dt)
+		for sprite in drawable:
+			sprite.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
+		for ast in asteroid:
+			if player.collision(ast):
+				print("Game Over!")
+				sys.exit()
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
